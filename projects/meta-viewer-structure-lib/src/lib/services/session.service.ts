069595b8c879f461c19ec5642aedff1449ffa073
Jesus Pastor <jnavalon@biit-solutions.com>
2024-12-14 16:52:14 +0100

    add session storage service

---
+++ projects/meta-viewer-structure-lib/src/lib/services/session.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {SessionService as AuthSessionService} from "authorization-services-lib";
+import {Constants} from "../constants";
+import {AuthService} from "./auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService extends AuthSessionService{
+
+  constructor(authService: AuthService) {
+    super(authService, Constants.SESSION_STORAGE.CONTEXT);
+  }
+}
