8a3e935a2456185343fb76f57b170b9f3d03ddd3
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-06-11 11:58:42 +0200

    Nca is public

--- projects/meta-viewer-structure-lib/src/public-api.ts
+++ projects/meta-viewer-structure-lib/src/public-api.ts
@@ -11,6 +11,7 @@ export * from './lib/models/value';
 
 export * from './lib/services/caching.service';
 export * from './lib/services/cadt.service';
+export * from './lib/services/nca.service';
 export * from './lib/services/meta-viewer-structure-root.service';
 export * from './lib/services/auth.service';
 export * from './lib/services/session.service';
