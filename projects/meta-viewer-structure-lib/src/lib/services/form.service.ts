a7f88941fdb43d78dab88cc95091b54fd202e68e
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-06-12 14:07:13 +0200

    Generalized client

--- projects/meta-viewer-structure-lib/src/lib/services/form.service.ts
+++ projects/meta-viewer-structure-lib/src/lib/services/form.service.ts
@@ -7,23 +7,23 @@ import {HttpClient} from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
-export class NcaService {
-  private static readonly ROOT_PATH: string = '/nca'
+export class FormService {
+  private static readonly ROOT_PATH: string = '/forms'
 
   constructor(private rootService: MetaViewerStructureRootService, private httpClient: HttpClient) { }
 
-  getJSON(): Observable<Collection> {
+  getJSON(formName:string): Observable<Collection> {
     return this.httpClient.get<Collection>(
-      `${this.rootService.serverUrl}${NcaService.ROOT_PATH}/json`);
+      `${this.rootService.serverUrl}${FormService.ROOT_PATH}/${formName}/json`);
   }
 
-  getXML(): Observable<XMLDocument> {
+  getXML(formName:string): Observable<XMLDocument> {
     return this.httpClient.get<XMLDocument>(
-      `${this.rootService.serverUrl}${NcaService.ROOT_PATH}/xml`);
+      `${this.rootService.serverUrl}${FormService.ROOT_PATH}/${formName}/xml`);
   }
 
-  regenerate(): Observable<void> {
+  regenerate(formName:string): Observable<void> {
     return this.httpClient.put<void>(
-      `${this.rootService.serverUrl}${NcaService.ROOT_PATH}/refresh`, undefined);
+      `${this.rootService.serverUrl}${FormService.ROOT_PATH}/${formName}/refresh`, undefined);
   }
 }
